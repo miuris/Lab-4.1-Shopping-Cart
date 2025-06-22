262b0dfd743d4d999217052fb17d642d036da2cd
Miuris.G <miuris.gutierrez@gmail.com>
2025-06-21 22:40:44 -0400

    Added Task2 and Task3

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//////////////
+//TASK 1 /////////////////
+////////////
 function capitalize(name) {
     if (!name) return '';
     return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
@@ -18,3 +21,28 @@ console.log(formatFullName('miuris', 'gutierrez'));
 console.log(formatFullName('', 'gutierrez'));
 console.log(formatFullName('miuris', ''));
 console.log(formatFullName('', ''));
+
+////////////
+//TASK 2 /////////////////
+//////////
+function calculateTotalCost(price, quantity, taxRate) {
+
+    if (typeof price !== 'number' || typeof quantity !== 'number' || typeof taxRate !== 'number') {
+        return "Invalid input.";
+    }
+    const totalCost = (price * quantity) * (1 + taxRate);
+
+    return totalCost;
+}
+////////////
+//TASK 3 /////////////////
+//////////
+function checkEligibility(age, isEmployed) {
+    if (age >18 && isEmployed) {
+        return "Eligible";
+    } else if (age >18 && !isEmployed) {
+        return "Conditionally Eligible";
+    } else {
+        return "Not Eligible";
+    }
+}
